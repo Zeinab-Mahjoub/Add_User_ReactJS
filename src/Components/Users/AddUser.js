1f6af796bc2e9f6e6dce6e4bb41b23ace5cb0638
zmahjoub <zmahjoub99@gmail.com>
2023-06-03 20:36:28 +0330

    feat: added fetch user data functionality for AddUser component

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,19 +1,50 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredAge.trim().length === 0 ||
+      +enteredAge < 1
+    ) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          value={enteredUsername}
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
+        <input
+          value={enteredAge}
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
